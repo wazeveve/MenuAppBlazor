083b8db301f7d01e4f9f30722d6dc6bfbded94cc
wazeveve <gabrielveanancio1123@gmail.com>
2025-11-16 15:04:08 -0300

    Creating context.cs, with Constructor, Relationship and the DB set instances

--- MenuAppBlazor/Models/Dish.cs
+++ MenuAppBlazor/Models/Dish.cs
@@ -2,5 +2,12 @@
 {
     public class Dish
     {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? ImageUrl { get; set; }
+        public double Price { get; set; }
+
+        public List<DishIngredient>? DishIngredients { get; set; } 
+
     }
 }
